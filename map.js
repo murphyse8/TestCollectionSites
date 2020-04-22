da9c821e91dc3ccfc7a9df6c4f820108c0770a47
JimGonsoski <james.gonsoski@metc.state.mn.us>
2020-04-22 17:51:29 -0500

    Add the popup.

--- map.js
+++ map.js
@@ -1,44 +1,60 @@
-var esriLeafletMap = function () {
-var init = function () {
-    var map = L.map('map').setView([46.5, -94], 6);
-    L.esri.basemapLayer('Topographic').addTo(map);
+var esriLeafletMap = (function () {
+  var init = function () {
+    var map = L.map("map").setView([46.5, -94], 6);
+    L.esri.basemapLayer("Topographic").addTo(map);
     //L.esri.basemapLayer('Streets').addTo(map);
 
     var siteIcon = L.icon({
-    iconUrl: 'MNMapPin.svg',
-    iconSize: [20, 20]
+      iconUrl: "MNMapPin.svg",
+      iconSize: [20, 20],
     });
 
-    map.createPane('sites');
+    map.createPane("sites");
 
     var testingSites = L.esri.featureLayer({
-    //url: 'https://services1.arcgis.com/KoDrdxDCTDvfgddz/arcgis/rest/services/MockupSites/FeatureServer/0',
-    url: 'https://services1.arcgis.com/KoDrdxDCTDvfgddz/ArcGIS/rest/services/CovidTestCollectionSites_Public/FeatureServer/0',
-    //url: 'https://services.arcgis.com/8ZpVMShClf8U8dae/ArcGIS/rest/services/TestingLocations_public/FeatureServer/0', //GISCorps Nation-wide map service
-    where: "1=1",
-    onEachFeature: function (feature, layer) {
+      //url: 'https://services1.arcgis.com/KoDrdxDCTDvfgddz/arcgis/rest/services/MockupSites/FeatureServer/0',
+      url:
+        "https://services1.arcgis.com/KoDrdxDCTDvfgddz/ArcGIS/rest/services/CovidTestCollectionSites_Public/FeatureServer/0",
+      //url: 'https://services.arcgis.com/8ZpVMShClf8U8dae/ArcGIS/rest/services/TestingLocations_public/FeatureServer/0', //GISCorps Nation-wide map service
+      where: "1=1",
+      onEachFeature: function (feature, layer) {
         console.dir(layer.feature);
-    },
-    pointToLayer: function (geojson, latlng) {
+      },
+      pointToLayer: function (geojson, latlng) {
         return L.marker(latlng, {
-        icon: siteIcon,
-        renderer: L.svg()
+          icon: siteIcon,
+          renderer: L.svg(),
         });
         // return L.circleMarker(latlng, {
         //   pane: 'sites',
         //   color: 'gray',
-        //   radius: 6, 
+        //   radius: 6,
         //   renderer: L.svg()
         // });
-    }
+      },
     });
     map.addLayer(testingSites);
-    testingSites.bindPopup(function (layer){
-    return L.Util.template('Testing Site<p></p>', layer.feature.properties);
+    testingSites.bindPopup(function (layer) {
+      return L.Util.template(
+        "<p>" +
+          "{ HealthSystem } <strong> { CollectSiteName }</strong >" +
+          "<br/>{ CollectAddress1 } { CollectAddress2 }" +
+          "<br/>{ City }, { State } { Zip }" +
+          "<br/><strong>Sun</strong> { HoursOfOpSun }" +
+          "<br/><strong>Mon</strong> { HoursOfOpMon }" +
+          "<br/><strong>Tue</strong> { HoursOfOpTues }" +
+          "<br/><strong>Wed</strong> { HoursOfOpWed }" +
+          "<br/><strong>Thu</strong> { HoursOfOpThurs }" +
+          "<br/><strong>Fri</strong> { HoursOfOpFri }" +
+          "<br/><strong>Sat</strong> { HoursOfOpSat }" +
+          "<br/><strong>Contact Info: { Phone }</strong>" +
+          "</p>",
+        layer.feature.properties
+      );
     });
-};
-return {
-    init: init
-};
-}();
+  };
+  return {
+    init: init,
+  };
+})();
 esriLeafletMap.init();

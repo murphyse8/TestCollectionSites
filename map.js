8408663f654dd5cd5290bdf9b8b5d76e22170c20
JimGonsoski <james.gonsoski@metc.state.mn.us>
2020-04-22 14:50:00 -0500

    Add sitelist.js reference and split out map.js

---
+++ map.js
@@ -0,0 +1,44 @@
+var esriLeafletMap = function () {
+var init = function () {
+    var map = L.map('map').setView([46.5, -94], 6);
+    L.esri.basemapLayer('Topographic').addTo(map);
+    //L.esri.basemapLayer('Streets').addTo(map);
+
+    var siteIcon = L.icon({
+    iconUrl: 'MNMapPin.svg',
+    iconSize: [20, 20]
+    });
+
+    map.createPane('sites');
+
+    var testingSites = L.esri.featureLayer({
+    //url: 'https://services1.arcgis.com/KoDrdxDCTDvfgddz/arcgis/rest/services/MockupSites/FeatureServer/0',
+    url: 'https://services1.arcgis.com/KoDrdxDCTDvfgddz/ArcGIS/rest/services/CovidTestCollectionSites_Public/FeatureServer/0',
+    //url: 'https://services.arcgis.com/8ZpVMShClf8U8dae/ArcGIS/rest/services/TestingLocations_public/FeatureServer/0', //GISCorps Nation-wide map service
+    where: "1=1",
+    onEachFeature: function (feature, layer) {
+        console.dir(layer.feature);
+    },
+    pointToLayer: function (geojson, latlng) {
+        return L.marker(latlng, {
+        icon: siteIcon,
+        renderer: L.svg()
+        });
+        // return L.circleMarker(latlng, {
+        //   pane: 'sites',
+        //   color: 'gray',
+        //   radius: 6, 
+        //   renderer: L.svg()
+        // });
+    }
+    });
+    map.addLayer(testingSites);
+    testingSites.bindPopup(function (layer){
+    return L.Util.template('Testing Site<p></p>', layer.feature.properties);
+    });
+};
+return {
+    init: init
+};
+}();
+esriLeafletMap.init();

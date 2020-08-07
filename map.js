292c329e82745dd0ea2480b70040a345eaf54215
JimGonsoski <james.gonsoski@metc.state.mn.us>
2020-08-07 15:19:58 -0500

    Remove CollectAddress2 from map pop-up.

--- map.js
+++ map.js
@@ -1,21 +1,21 @@
 var esriLeafletMap = (function () {
   var init = function () {
     var map = L.map("map",
-    {
-      center: [46.5, -94],
-      zoom: 6,
-      minZoom: 4,
-      scrollWheelZoom: false,
-      maxBounds: [[35,-105],[60,-80]],
-      maxBoundsViscosity: 1.0
-    });
+      {
+        center: [46.5, -94],
+        zoom: 6,
+        minZoom: 4,
+        scrollWheelZoom: false,
+        maxBounds: [[35, -105], [60, -80]],
+        maxBoundsViscosity: 1.0
+      });
     L.esri.basemapLayer("Topographic").addTo(map);
 
     var siteIcon = L.icon({
       iconUrl: "MNMapPin.svg",
       iconSize: [25, 25],
-      iconAnchor: [10,25],
-      popupAnchor: [3,-20]
+      iconAnchor: [10, 25],
+      popupAnchor: [3, -20]
     });
 
     map.createPane("sites");
@@ -35,26 +35,26 @@ var esriLeafletMap = (function () {
       var l = layer.feature.properties;
       if (l.HealthSystem === 'Sanford Health') {
         l.CollectAddress1 = "";
-        l.CollectAddress2 = "";
+        //l.CollectAddress2 = "";
       }
       var template =
-            "{ HealthSystem }" + 
-            "<br/><strong> { CollectSiteName }</strong> ";
+        "{ HealthSystem }" +
+        "<br/><strong> { CollectSiteName }</strong> ";
       if (l.CollectAddress1 && l.CollectAddress1.length > 0) {
         template += "<br/>{ CollectAddress1 } ";
-        if (l.CollectAddress2 && l.CollectAddress2.length > 0) {
-          template += "{ CollectAddress2 }";
-        }
+        //if (l.CollectAddress2 && l.CollectAddress2.length > 0) {
+        //    template += "{ CollectAddress2 }";
+        //  }
       }
       template +=
-            "<br/>{ City }, { State } { Zip }" +
-            "<br/><strong>Weekdays: </strong> { HoursOfOpMF }" +
-            "<br/><strong>Weekends: </strong> { HoursOfOpSatSun }";
+        "<br/>{ City }, { State } { Zip }" +
+        "<br/><strong>Weekdays: </strong> { HoursOfOpMF }" +
+        "<br/><strong>Weekends: </strong> { HoursOfOpSatSun }";
       template +=
-            "<br/><strong>Contact Info: <a href='tel: { Phone }'>{ Phone }</a></strong>";
+        "<br/><strong>Contact Info: <a href='tel: { Phone }'>{ Phone }</a></strong>";
       template +=
-            "<br/>{ DirUtilCol }" +
-            "<br/><button onclick='FilterSite(\"{ CollectSiteName }\")'>See more details below.</button>"
+        "<br/>{ DirUtilCol }" +
+        "<br/><button onclick='FilterSite(\"{ CollectSiteName }\")'>See more details below.</button>"
       return L.Util.template(template, l);
     }, { maxWidth: 300 });
     // testingSites.on('click', function (e) {
